e1cadfd600eca46fa430af37fbdbce96669a6f90
Ze Sun <zesun@ucsd.edu>
2025-02-28 14:53:58 -0800

    Added a subset to work wit

--- botV2.js
+++ botV2.js
@@ -12,19 +12,15 @@ await sleep(2000);
 const DATA = DB.data;
 const LANG = DB.languages;
 
-const es_words = DATA.filter(data => data.language == "es");
-console.log(DB);
+let ar = DATA.filter( data => data.language == 'ar');
 
-for (let i = 0; i < es_words.length; i++) {
-
-    let postData = {
-        "broadcaster_id": config.channel_id,
-        "sender_id": config.channel_base_id,
-        "message": `${es_words[i].text}`
-    };
-
+for (let i = 0; i < 5; i ++) {
     var {data} = await axios.post(config.url, 
-        postData, {
+        {
+            "broadcaster_id": config.channel_id,
+            "sender_id": config.channel_base_id,
+            "message": `${ar[i].text}`
+        }, {
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ${config.accessToken}`,
@@ -35,6 +31,48 @@ for (let i = 0; i < es_words.length; i++) {
     console.log(JSON.stringify(data));
     await sleep(1500);
 }
+
+// let subset = DATA.filter(data => ( data.language == 'da' || data.language == 'cs'
+//     || data.language == 'de'
+// ));
+
+// let curr_lang = '';
+
+// for (let i = 0; i < subset.length; i++) {
+
+//     let postData = {
+//         "broadcaster_id": config.channel_id,
+//         "sender_id": config.channel_base_id,
+//         "message": `${subset[i].text}`
+//     };
+//     if (subset[i].language != curr_lang) {
+//         await axios.post(config.url, 
+//             {
+//                 "broadcaster_id": config.channel_id,
+//                 "sender_id": config.channel_base_id,
+//                 "message": `${subset[i].language}`
+//             }, {
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                     "Authorization": `Bearer ${config.accessToken}`,
+//                     "Client-Id": `${config.client_id}`
+//                 }
+//         });
+//         curr_lang = subset[i].language;
+//         await sleep(1500);
+//     }
+//     var {data} = await axios.post(config.url, 
+//         postData, {
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Authorization": `Bearer ${config.accessToken}`,
+//                 "Client-Id": `${config.client_id}`
+//             }
+//     });
+
+//     console.log(JSON.stringify(data));
+//     await sleep(1500);
+// }
 
 
 
