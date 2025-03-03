6528c8ac0e46bba6f3ee3ca4ab0b04f6f5e05849
Ze Sun <zesun@ucsd.edu>
2025-03-03 15:27:40 -0800

    Finalized bot

--- botV2.js
+++ botV2.js
@@ -9,17 +9,21 @@ function sleep(ms) {
 const DB = await loadData(config.paths);
 await sleep(2000);
 
-const DATA = DB.data;
-const LANG = DB.languages;
+const LANG = DB.languages; // 12 languages
+const DATA = {};
 
-let ar = DATA.filter( data => data.language == 'ar');
+for (let lang of LANG) {
+    DATA[lang] = DB.data.filter(e => e.language == lang);
+    console.log(lang);
+}
 
-for (let i = 0; i < 5; i ++) {
+await sleep(1000);
+for (let lang of LANG) {
     var {data} = await axios.post(config.url, 
         {
             "broadcaster_id": config.channel_id,
             "sender_id": config.channel_base_id,
-            "message": `${ar[i].text}`
+            "message": `${lang.toUpperCase()}`
         }, {
             headers: {
                 "Content-Type": "application/json",
@@ -30,7 +34,28 @@ for (let i = 0; i < 5; i ++) {
 
     console.log(JSON.stringify(data));
     await sleep(1500);
+    for (let i = 0; i < DATA[lang].length; i ++) {
+        if (i >= 5) {
+            continue;
+        }
+        var {data} = await axios.post(config.url, 
+            {
+                "broadcaster_id": config.channel_id,
+                "sender_id": config.channel_base_id,
+                "message": `${DATA[lang][i].text}`
+            }, {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${config.accessToken}`,
+                    "Client-Id": `${config.client_id}`
+                }
+        });
+    
+        console.log(JSON.stringify(data));
+        await sleep(1500);
+    }
 }
+
 
 // let subset = DATA.filter(data => ( data.language == 'da' || data.language == 'cs'
 //     || data.language == 'de'
